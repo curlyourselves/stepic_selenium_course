db214676f9b7f44557f424685d0bd34065beb5f0
rba <romik200298@mail.ru>
2023-04-06 17:56:34 +0200

    Commit and push a few files

---
+++ 2_3_6.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    butt = browser.find_element(By.CSS_SELECTOR, 'button')
+    butt.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#input_value')
+    x = x_element.text
+    y = calc(int(x))
+
+    field = browser.find_element(By.CSS_SELECTOR, '#answer')
+    field.send_keys(y)
+
+    button = browser.find_element(By.TAG_NAME, "button")
+    button.click()
+
+finally:
+    time.sleep(5)
+    browser.quit()
